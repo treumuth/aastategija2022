0a07d0c6ffd4e9bd156f718004557bdb81c70f13
treumuth <58808087+treumuth@users.noreply.github.com>
2022-03-10 07:56:49 +0200

    sain korda selle jookide värgi
    ei pea enam jooke eraldi uurima

--- voistlus.py
+++ voistlus.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
-# tahtsin andmed objektidesse panna, siis saab JSON-i hiljem ühe reaga
+# andmed objektidesse, siis saab JSON-i hiljem ühe reaga
 class Grupp:
     def __init__(self, nimetus, toidud):
         self.nimetus = nimetus
@@ -16,16 +16,14 @@ class Toit:
 
 # url = "https://siseveeb.voco.ee/veebivormid/sookla_menuu"
 url = 'http://192.168.22.172/menu-example/'
-
 sisu = requests.get(url).text
 doc = BeautifulSoup(sisu, "html.parser")
 
 pealkirjad = doc.find_all("div", {"class": "panel-heading text-center"})
 
 grupilist = []
-# kõik kuni mahladeni on OK
-for pealkiri in pealkirjad[:3]:
-    toidud = pealkiri.next_sibling
+for pealkiri in pealkirjad:
+    toidud = pealkiri.parent.find_all("li")
     toidulist = []
     for toit in toidud:
         detailid = toit.find_all("h2")
@@ -34,23 +32,9 @@ for pealkiri in pealkirjad[:3]:
     m = Grupp(pealkiri.text, toidulist)
     grupilist.append(m)
 
-# mahladega eraldi jamamine, sest ma ei viitsinud enam
-toidud = pealkirjad[3].next_sibling
-for toit in toidud:
-    joogid = toit.find_all("li")
-    toidulist = []
-    for jook in joogid:
-        t = Toit(jook.h2.contents[0], jook.h2.span.text.strip("€"), "")
-        toidulist.append(t)
-    m = Grupp(pealkirjad[3].text, toidulist)
-    grupilist.append(m)
-
 def obj_dict(obj):
     return obj.__dict__
 
-# kuna mul objektides kõik, siis JSON tehakse mugavalt ühe reaga
-json_string = json.dumps(grupilist, default=obj_dict, indent=4)
+with open('menu.json', 'w') as f:
+    json.dump(grupilist, f, default=obj_dict, indent=4)
 
-fail = open("menu.json", "w", encoding="utf8")
-fail.write(json_string)
-fail.close()
